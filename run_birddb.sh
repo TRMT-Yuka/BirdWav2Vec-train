018e833dd2218cde82af33757db56fd3a0b65447
Ryosuke Kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2023-05-30 11:00:09 +0900

    initial commit

---
+++ run_birddb.sh
@@ -0,0 +1,22 @@
+accelerate launch run_wav2vec2_pretraining_no_trainer.py \
+	--dataset_name="kojima-r/birddb_small" \
+	--dataset_split_names train \
+	--dataset_config_names clean \
+	--validation_split_percentage 10 \
+	--model_name_or_path="patrickvonplaten/wav2vec2-base-v2" \
+	--output_dir="./wav2vec2-birddb" \
+	--max_train_steps="20000" \
+	--num_warmup_steps="32000" \
+	--learning_rate="0.005" \
+	--weight_decay="0.01" \
+	--max_duration_in_seconds="100.0" \
+	--min_duration_in_seconds="0.2" \
+	--logging_steps="100" \
+	--saving_steps="100000" \
+	--per_device_train_batch_size="32" \
+	--per_device_eval_batch_size="32" \
+	--adam_beta1="0.9" \
+	--adam_beta2="0.98" \
+	--adam_epsilon="1e-06" \
+	--gradient_checkpointing \
+
